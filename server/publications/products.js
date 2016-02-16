967a3f8cff528a2b405f8ecdc540e62bf597777e
Santiago Quinteros <contacto.quinteros@gmail.com>
2016-02-16 15:00:43 -0300

    Modify pulications to only return a list of ids of the products, and add a publication to get an individual product

--- server/publications/products.js
+++ server/publications/products.js
@@ -3,5 +3,10 @@ Meteor.publish('products',(category)=>{
 	if(!_.contains(Constants.categories,category)){
 		return Products.find({});
 	}
-	return Products.find({category:category});
+	return Products.find({category:category},{fields:{_id:1}});
+})
+
+Meteor.publish('product',(productId)=>{
+	check(productId,String);
+	return Products.find({_id:productId});
 })
